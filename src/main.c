7304a23039228bd95b21d2a02772a4d777575ed6
gulser <glserozkan@gmail.com>
2024-01-15 08:30:15 +0100

    leaks branch is created

--- src/main.c
+++ src/main.c
@@ -16,9 +16,9 @@ double	position_to_degree(char p_direction)
 void	init_player(t_data *data)
 {
 	data->player = (t_player *)malloc(sizeof(t_player));
-    data->player->x = data->file.map.player_pos_x;
-    data->player->y = data->file.map.player_pos_y;
-    data->player->th = position_to_degree(data->file.map.p_direction);
+	data->player->x = data->file.map.player_pos_x;
+	data->player->y = data->file.map.player_pos_y;
+	data->player->th = position_to_degree(data->file.map.p_direction);
 			printf("**-----------------------------**\n");
 			printf("*	data.px	: %f	*\n", data->player->x);
 			printf("*	data.py	: %f	*\n", data->player->y);
@@ -26,24 +26,27 @@ void	init_player(t_data *data)
 			printf("**-----------------------------**\n");
 }
 
+// void f()
+// {
+// 	system("leaks cub3d");
+// }
+
 int main(int argc, char **argv)
 {
 	t_data		data;
-	//mlx_t		*mlx;
-	// mlx_image_t	*image;
 
-    if (!(data.mlx = mlx_init(SX, SY, "cub3d", true)))
+	if (!(data.mlx = mlx_init(SX, SY, "cub3d", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-    if (!(data.image = mlx_new_image(data.mlx, SX, SY)))
+	if (!(data.image = mlx_new_image(data.mlx, SX, SY)))
 	{
 		mlx_close_window(data.mlx);
 		puts(mlx_strerror(mlx_errno));
 		return(EXIT_FAILURE);
 	}
-    if (mlx_image_to_window(data.mlx, data.image, 0, 0) == -1)
+	if (mlx_image_to_window(data.mlx, data.image, 0, 0) == -1)
 	{
 		mlx_close_window(data.mlx);
 		puts(mlx_strerror(mlx_errno));
@@ -55,8 +58,10 @@ int main(int argc, char **argv)
 		//err_msg("Parser has failded");
 	init_player(&data);
 	//mlx_key_hook(mlx, display, &data);
-     mlx_key_hook(data.mlx, key_press, &data);
-    mlx_loop(data.mlx);
+	 mlx_key_hook(data.mlx, key_press, &data);
+	mlx_loop(data.mlx);
 	free(data.player);
-    mlx_terminate(data.mlx);
+	// while (1)
+	// 	atexit(f);
+	mlx_terminate(data.mlx);
 }
